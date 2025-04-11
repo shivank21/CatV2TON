18ad337e2b33bf22eb1008ed272cbbfe213e49a6
Shivank Garg <128126577+shivank21@users.noreply.github.com>
2025-04-11 19:54:32 +0530

    use relative paths

--- run.sh
+++ run.sh
@@ -11,14 +11,18 @@ gdown 12QDkjn30P9EiIqZhtCFL4pEi7oZj2psQ
 
 tar -xvzf /content/CatV2TON/ViViD-CLIPED-512x384-Test.tar.gz
 
+Data_Path="./ViViD-CLIPED-512x384-Test"
+Output="./results"
+
 CUDA_VISIBLE_DEVICES=0 python eval_video_try_on.py \
 --dataset vivid \
---data_root_path "/content/CatV2TON/ViViD-CLIPED-512x384-Test"\
---output_dir "/content/CatV2TON/results" \
+--data_root_path "$Data_Path" \
+--output_dir "$Output" \
 --dataloader_num_workers 1 \
 --batch_size 1 \
 --seed 42 \
 --mixed_precision bf16 \
 --allow_tf32 \
 --repaint \
---eval_pair  
+--eval_pair
+
