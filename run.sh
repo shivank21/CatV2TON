13c8ac5cda0cc7e576ef7d2432ab1d578dfa8b99
Shivank Garg <128126577+shivank21@users.noreply.github.com>
2025-04-11 19:52:46 +0530

    add req and run

---
+++ run.sh
@@ -0,0 +1,24 @@
+git clone https://github.com/shivank21/CatV2TON.git
+cd CatV2TON
+
+conda create -n catv2ton python=3.11
+
+conda activate catv2ton
+
+pip install -r requirements.txt
+
+gdown 12QDkjn30P9EiIqZhtCFL4pEi7oZj2psQ
+
+tar -xvzf /content/CatV2TON/ViViD-CLIPED-512x384-Test.tar.gz
+
+CUDA_VISIBLE_DEVICES=0 python eval_video_try_on.py \
+--dataset vivid \
+--data_root_path "/content/CatV2TON/ViViD-CLIPED-512x384-Test"\
+--output_dir "/content/CatV2TON/results" \
+--dataloader_num_workers 1 \
+--batch_size 1 \
+--seed 42 \
+--mixed_precision bf16 \
+--allow_tf32 \
+--repaint \
+--eval_pair  
